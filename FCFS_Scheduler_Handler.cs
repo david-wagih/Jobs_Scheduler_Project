06dab55a08ba1ce5cf4340734be69a8714b9c5ab
Mark-GeorgeK <79061147+Mark-GeorgeK@users.noreply.github.com>
2022-04-11 16:24:33 +0200

    FCFS_Scheduler_Handler.cs file added file added with a single function to handle the first come first served scheduling technique for processes.

---
+++ FCFS_Scheduler_Handler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+int[] FCFS_Scheduler_Handler(int[] arrival_time, int[] burst_time)
+{
+    SortedDictionary<int, int> Processes = new SortedDictionary<int, int>();
+    for (int i = 0; i < arrival_time.Length; i++)
+        Processes[arrival_time[i]] = burst_time[i];
+
+    int c = 0;
+    int[] result = new int[arrival_time.Length];
+    foreach (var process in Processes)
+        result[c++] = process.Value;
+
+    return result;
+}
