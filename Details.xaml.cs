8430e89758cf23cff9e63b1b92f06a6798a9f1a8
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-04-13 15:08:57 +0200

    Creating the Home Window and Process Details Window to take input from user

---
+++ Details.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JobsScheduler
+{
+    /// <summary>
+    /// Interaction logic for Details.xaml
+    /// </summary>
+    public partial class Details : Window
+    {
+
+        public Details(int Number , string type)
+        {
+            InitializeComponent();
+            try
+            {
+                RenderRows(Number, type);
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void RenderRows(int number, string type)
+        {
+            for(int i = 0; i < number; i++)
+            {
+                if (type == "FCFS" || type == "SJF Preemptive" || type == "SJF Non-Preemptive")
+                {
+                    for (int j = 0; j < ourGrid.ColumnDefinitions.Count-2; j++)
+                    {
+
+
+                        Border panel = new Border();
+                        Grid.SetColumn(panel, j);
+                        Grid.SetRow(panel, i);
+                        // Text Box Properties
+                        TextBox textBox = new TextBox();
+                        if(j == 0)
+                        {
+                            textBox.Text = "P" + i;
+                            textBox.IsReadOnly = true;
+                        }
+                        textBox.Width = 100;
+                        textBox.TextAlignment = TextAlignment.Center;
+                        textBox.HorizontalAlignment = HorizontalAlignment.Center;
+                        textBox.VerticalAlignment = VerticalAlignment.Center;
+                        panel.Child = textBox;
+                        ourGrid.Children.Add(panel);
+
+                    }
+                }
+                else if( type == "Priority Preemptive" || type == "Priority Non-Preemptive")
+                {
+                    for (int j = 0; j < ourGrid.ColumnDefinitions.Count-1; j++)
+                    {
+
+                        Border panel = new Border();
+                        Grid.SetColumn(panel, j);
+                        Grid.SetRow(panel, i);
+                        // Text Box Properties
+                        TextBox textBox = new TextBox();
+                        if (j == 0)
+                        {
+                            textBox.Text = "P" + i;
+                            textBox.IsReadOnly = true;
+                        }
+                        textBox.Width = 100;
+                        textBox.TextAlignment = TextAlignment.Center;
+                        textBox.HorizontalAlignment = HorizontalAlignment.Center;
+                        textBox.VerticalAlignment = VerticalAlignment.Center;
+                        panel.Child = textBox;
+                        ourGrid.Children.Add(panel);
+
+                    }
+
+                }
+                else
+                // this is the case for Round Robin
+                {
+                    for (int j = 0; j < ourGrid.ColumnDefinitions.Count; j++)
+                    {
+                        if(j == 3)
+                        {
+                            continue;
+                        }
+                        Border panel = new Border();
+                        Grid.SetColumn(panel, j);
+                        Grid.SetRow(panel, i);
+                        // Text Box Properties
+                        TextBox textBox = new TextBox();
+                        if (j == 0)
+                        {
+                            textBox.Text = "P" + i;
+                            textBox.IsReadOnly = true;
+                        }
+                        textBox.Width = 100;
+                        textBox.TextAlignment = TextAlignment.Center;
+                        textBox.HorizontalAlignment = HorizontalAlignment.Center;
+                        textBox.VerticalAlignment = VerticalAlignment.Center;
+                        panel.Child = textBox;
+                        ourGrid.Children.Add(panel);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+ 
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,52 @@ namespace JobsScheduler
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int noOfProcesses = 0;
+        public string schedulerType = null;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+
+        private void Next_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int NumberOfProcesses = Int32.Parse(ProcessesNo_Input.Text);
+                if (NumberOfProcesses < 1 || NumberOfProcesses > 5)
+                {
+                    errorMessage.Text = "please enter numbers only between 1 and 5";
+
+
+                }
+                // want to check if the user has chosen something from the combo box
+                else if (Combo_Value.Text == "")
+                {
+                    errorMessage.Text = "please choose one from the above Schedulers";
+                }
+                else
+                {
+                    // need to navigate to new window after updating the public variable of no of processes
+                    noOfProcesses = NumberOfProcesses;
+                    schedulerType = Combo_Value.Text;
+                    var newForm = new Details(noOfProcesses, schedulerType); //create your new form.
+                    newForm.Show(); //show the new form.
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+            
+        }
+
+        // this to ensure that user can't type any string in the text Box or any number greater than 5
+        private void ProcessesNo_Input_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
         }
     }
 }
