ec0be243017ef94085e2670d3be4f69abda0503d
Mark-GeorgeK <79061147+Mark-GeorgeK@users.noreply.github.com>
2022-04-14 14:14:50 +0200

    Encapsulating function in a class added the Handler function to the FCFS_Scheduler class

---
+++ FCFS_Scheduler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+static class FCFS_Scheduler {
+    static int[] FCFS_Scheduler_Handler(int[] arrival_time, int[] burst_time) {
+        SortedDictionary<int, int> Processes = new SortedDictionary<int, int>();
+        for (int i = 0; i < arrival_time.Length; i++)
+            Processes[arrival_time[i]] = burst_time[i];
+
+        int c = 0;
+        int[] result = new int[arrival_time.Length];
+        foreach (var process in Processes)
+            result[c++] = process.Value;
+
+        return result;
+    }
+}
