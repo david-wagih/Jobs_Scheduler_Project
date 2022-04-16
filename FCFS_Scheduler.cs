6a1f741569b3ac771bc16e171253e38762913292
Mark-GeorgeK <79061147+Mark-GeorgeK@users.noreply.github.com>
2022-04-16 15:15:59 +0200

    Update FCFS_Scheduler.cs

--- FCFS_Scheduler.cs
+++ FCFS_Scheduler.cs
@@ -1,16 +1,60 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-static class FCFS_Scheduler {
-    static int[] FCFS_Scheduler_Handler(int[] arrival_time, int[] burst_time) {
-        SortedDictionary<int, int> Processes = new SortedDictionary<int, int>();
-        for (int i = 0; i < arrival_time.Length; i++)
-            Processes[arrival_time[i]] = burst_time[i];
+public class ProcessWrapper : JobsScheduler.Process, IComparable<ProcessWrapper>
+{
+    public ProcessWrapper(JobsScheduler.Process p)
+    {
+        this.arrivalTime = p.arrivalTime;
+        this.burstTime = p.burstTime;
+    }
 
-        int c = 0;
-        int[] result = new int[arrival_time.Length];
-        foreach (var process in Processes)
-            result[c++] = process.Value;
+    int IComparable<ProcessWrapper>.CompareTo(ProcessWrapper other)
+    {
+        if (this.arrivalTime >= other.arrivalTime) return 1;
+        else return -1;
+    }
+}
 
-        return result;
+public class FCFS_Scheduler
+{
+
+    public List<JobsScheduler.outputProcesses> outputList;
+    public float averageWaitingTime;
+
+    public FCFS_Scheduler(List<JobsScheduler.Process> processes)
+    {
+        outputList = new List<JobsScheduler.outputProcesses>();
+        averageWaitingTime = 0;
+
+        List<ProcessWrapper> OrderOfExecution = new List<ProcessWrapper>();
+        foreach (var process in processes)
+        {
+            ProcessWrapper pw = new ProcessWrapper(process);
+            OrderOfExecution.Add(pw);
+        }
+
+        OrderOfExecution.Sort();
+
+        float acc = 0;
+        foreach (var process in OrderOfExecution)
+        {
+            acc += process.burstTime;
+            averageWaitingTime += acc;
+        }
+        averageWaitingTime -= acc;
+        averageWaitingTime = (float)averageWaitingTime / OrderOfExecution.Count;
+
+        int startingTime = 0;
+        foreach (var process in OrderOfExecution)
+        {
+            JobsScheduler.outputProcesses op = new JobsScheduler.outputProcesses();
+            op.processNumber = process.processNumber;
+            op.startTime = startingTime;
+            op.usingTime = process.burstTime;
+            this.outputList.Add(op);
+            startingTime += process.burstTime;
+        }
+
     }
 }
