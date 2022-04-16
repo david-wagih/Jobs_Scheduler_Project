41567f7ce8e544343581148393c139bcb36b80f6
Mark-GeorgeK <79061147+Mark-GeorgeK@users.noreply.github.com>
2022-04-16 14:52:45 +0200

    Changed list field type to outputProcesses

--- FCFS_Scheduler.cs
+++ FCFS_Scheduler.cs
@@ -19,12 +19,15 @@ public class ProcessWrapper : JobsScheduler.Process, IComparable<ProcessWrapper>
 public class FCFS_Scheduler
 {
 
-    public List<JobsScheduler.Process> OrderOfExecution;
+    public List<JobsScheduler.outputProcesses> outputList;
     public float averageWaitingTime;
 
     public FCFS_Scheduler(List<JobsScheduler.Process> processes)
     {
-        OrderOfExecution = new List<JobsScheduler.Process>();
+        outputList = new List<JobsScheduler.outputProcesses>();
+        averageWaitingTime = 0;
+
+        List<ProcessWrapper> OrderOfExecution = new List<ProcessWrapper>();
         foreach (var process in processes)
         {
             ProcessWrapper pw = new ProcessWrapper(process);
@@ -33,7 +36,6 @@ public class FCFS_Scheduler
 
         OrderOfExecution.Sort();
 
-        averageWaitingTime = 0;
         float acc = 0;
         foreach (var process in OrderOfExecution)
         {
@@ -42,5 +44,17 @@ public class FCFS_Scheduler
         }
         averageWaitingTime -= acc;
         averageWaitingTime = (float)averageWaitingTime / OrderOfExecution.Count;
+
+        int startingTime = 0;
+        foreach (var process in OrderOfExecution)
+        {
+            JobsScheduler.outputProcesses op = new JobsScheduler.outputProcesses();
+            op.processNumber = process.processNumber;
+            op.startTime = startingTime;
+            op.usingTime = process.burstTime;
+            this.outputList.Add(op);
+            startingTime += process.burstTime;
+        }
+
     }
 }
